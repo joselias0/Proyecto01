e654e0d2209da9a3c1c5ca1111609d9ddde5b000
joselias0 <99055413+joselias0@users.noreply.github.com>
2022-04-08 20:19:35 -0600

    Update Alimento.java

--- Alimento.java
+++ Alimento.java
@@ -11,21 +11,103 @@ package proyecto01;
 //Clase hija que hereda de la clase padre Productos
 public class Alimento extends Productos 
 {   //variable unica para la sub clase Higiene
+    private int id;
+    private String nombre;
+    private float precio;
+    private int cantidad;
+    private String tipo;
     private String caducidad;
-    public Alimento(){
-        super();
-    }
-    public Alimento(String nombre,int precio,int cantidad,String tipo){
-        super();
-    }
-    
-    //Metodo utilizado para saber la disponibilidad de productos Alimenticios
 
+    //Metodo constructor sin parametros
+    public Alimento() {
+    }
+
+     //insertar datos excluyendo el ID
+    public Alimento(String nombre, float precio, int cantidad, String tipo, String caducidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+        this.caducidad = caducidad;
+    }
+
+     //insertar datos 
+    public Alimento(int id, String nombre,float precio,int cantidad, String tipo, String caducidad) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.id = id;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+        this.caducidad = caducidad;
+    }
     
+    
+    //Mostrar el nombre
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Modificar el nombre
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Mostrar el precio
+    @Override
+    public float getPrecio() {
+        return precio;
+    }
+
+    //Modificar el precio
+    @Override
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    //Mostrar el ID 
+    @Override
+    public int getId() {
+        return id;
+    }
+
+    //Modificar ID
+    @Override
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Mostrar la cantidad
+    @Override
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    //Modificar la cantidad
+    @Override
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    //Mostrar el tipo
+    @Override
+    public String getTipo() {
+        return tipo;
+    }
+
+    //Modificar el tipo
+    @Override
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    //Mostrar la caducidad
     public String getCaducidad() {
         return caducidad;
     }
 
+    //Modificar la caducidad
     public void setCaducidad(String caducidad) {
         this.caducidad = caducidad;
     }
