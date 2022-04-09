1b46109ac45b14b0c4cb2faa5d1ded4e6832dda1
joselias0 <99055413+joselias0@users.noreply.github.com>
2022-04-08 20:36:03 -0600

    Create interface_perros.java

---
+++ interface_perros.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyecto01;
+
+import java.util.List;
+
+
+public interface interface_perros<T> {
+  
+    public int update (int id, String nombre, String raza, String color,String dFisica);
+  
+}
