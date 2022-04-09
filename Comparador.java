11b69d111c6f1a3b81419a77bcbc5e49fd8b9523
joselias0 <99055413+joselias0@users.noreply.github.com>
2022-04-08 20:32:06 -0600

    Create Comparador.java

---
+++ Comparador.java
@@ -0,0 +1,15 @@
+package proyecto01;
+
+
+ 
+public interface Comparador {
+    public boolean igualQue(Productos o);
+    
+    public boolean menorQue(Productos o);
+    
+    public boolean menorIgual(Productos o);
+    
+    public boolean mayorQue(Productos o);
+    
+    public boolean mayorIgual(Productos o);
+}
