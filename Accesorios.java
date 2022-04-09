c7cccbc46e9bf1e3fcec35a0e6faec34c499a5f1
joselias0 <99055413+joselias0@users.noreply.github.com>
2022-04-08 20:20:11 -0600

    Update Accesorios.java

--- Accesorios.java
+++ Accesorios.java
@@ -11,20 +11,93 @@ package proyecto01;
 //Clase hija que hereda de la clase padre Productos
 public class Accesorios extends Productos 
 {   //variable unica para la sub clase Accesorio
+    private int id;
+    private String nombre;
+    private float precio;
+    private int cantidad;
+    private String tipo;
     private String tAccesorio;
-    public Accesorios(){
-        super();
+
+     //insertar datos excluyendo el ID
+    public Accesorios(int id, String nombre, float precio, int cantidad, String tipo, String tAccesorio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+        this.tAccesorio = tAccesorio;
     }
-    public Accesorios(String nombre,int precio,int cantidad,String tipo){
-        super();
+
+     //insertar datos 
+    public Accesorios(String nombre, float precio, int cantidad, String tipo, String tAccesorio) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.tipo = tipo;
+        this.tAccesorio = tAccesorio;
     }
     
-    //Metodo utilizado para saber la disponibilidad de productos del tipo Accesorios
+    //Metodo constructor sin parametros
+    public Accesorios(){
+       
+    }
+    
+    //Mostrar el ID
+    public int getId() {
+        return id;
+    }
 
+    //Modificar el ID
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Mostrar el nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    //Modificar el nombre
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //Mostrar el precio
+    public float getPrecio() {
+        return precio;
+    }
+
+    //Modificar el precio
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    //Mostrar la cantidad
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    //Modificar la cantidad
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    //Mostrar el tipo
+    public String getTipo() {
+        return tipo;
+    }
+
+    //Modificar el tipo
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    //Mostrar el tipo de accesorio
     public String gettAccesorio() {
         return tAccesorio;
     }
 
+    //Modificar el tipo de accesorio
     public void settAccesorio(String tAccesorio) {
         this.tAccesorio = tAccesorio;
     }
