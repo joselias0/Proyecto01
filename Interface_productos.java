ecd38d1cd3f2f1a8adea2d4de498c002cde25694
joselias0 <99055413+joselias0@users.noreply.github.com>
2022-04-08 20:34:23 -0600

    Create Interface_productos.java

---
+++ Interface_productos.java
@@ -0,0 +1,6 @@
+package proyecto01;
+
+
+public interface Interface_productos {
+     public int update (int id, String nombre, float precio, int cantidad, String tipo, float higiene, String Alimento, String Accesorios);
+}
